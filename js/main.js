2a7eae872d741f854cf26cbc01f50b90998b85d3
HuugoLugo <hugolugo.hrn@gmail.com>
2023-10-30 16:30:55 -0600

    Ejercicio de práctica-Clase2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-//Inicia--Recuerda que const no puede modificarse, let si
-const ESPACIO_BLANCO = " ";
-let textoParaNombre = "Increiblemente";
-let textoUnion = "tiene";
-let textoParaAños = "años sin bañarse";
+/*Inicia--Recuerda que const no puede modificarse, let si, aqui coloca la pregunta (parsea para que tome numero), 
+y las respuestas que se le darán.*/
+const EVALUA_SERVICIO = parseInt(prompt("Por favor evalúe nuestro servicio, ingrese un valor entre 0 y 10 (siendo 10 lo más alto). Si nuestro servicio es excelente escriba el valor 100 "));
 
+let excelenteServicio = "Usted es sabio, y gracias a ello, se ha ganado un descuento de $1000.";
+let buenServicio = "Le agradecemos su valoración, queremos seguir ayudandolo a conseguir sus objetivos.";
+let malServicio = "Lamentamos esa valoración, esperamos mejorar en su servicio, escribanos a ayudita@noleayudo.com, para poder atenderlo mejor.";
+let noLee = "Hola, lo invito a leer bien, eso no es lo que le pedimos, intente otra vez.";
 
-/*Aquí vas a pedir nombre y años, para después concatenarlos con las variables anteriores
+/*Aquí vas a escribir las condiciones que se deben cumplir para entregar distintas respuestas
 y mostrarlas con un alert y / o log*/
-let ingresaNombre = prompt("Ingrese su nombre");
-let ingresaAños = prompt("¿Cuántos años tiene?");
-
-let sentenciaCompleta = textoParaNombre + ESPACIO_BLANCO + ingresaNombre + ESPACIO_BLANCO + textoUnion + ESPACIO_BLANCO + ingresaAños + ESPACIO_BLANCO + textoParaAños;
-console.log(sentenciaCompleta);
-alert(sentenciaCompleta);
+if (isNaN(EVALUA_SERVICIO)) { alert("Ese ni es un número, intente otra vez."); }
+else {
+    if (EVALUA_SERVICIO === 100) { alert(excelenteServicio); }
+    else if (EVALUA_SERVICIO >= 6 && EVALUA_SERVICIO <= 10) { alert(buenServicio); }
+    else if (EVALUA_SERVICIO <= 5) { alert(malServicio); }
+    else { alert(noLee); }
+}
