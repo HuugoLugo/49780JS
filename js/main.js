5bf7b6d1ca6d659465c0efc63afb388e997a94e4
HuugoLugo <hugolugo.hrn@gmail.com>
2023-11-07 16:47:35 -0600

    preEntrega1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,47 @@
-//Inicia--Recuerda que const no puede modificarse, let si
-const espacioBlanco = " ";
-let textoParaNombre = "Increiblemente";
-let textoUnion = "tiene";
-let textoParaAños = "años sin bañarse";
+/*Esto te muestra como hacer compras de productos, aplicando descuento de ser necesario
+se hace uso de do while, switch, y una función para calcular el descuento*/
+let totalAmount = 0;
+let amount;
+let continueDoing;
+let discountAmount;
 
+do {
+    const PRODUCTS = prompt("Ingrese el producto a comprar (pan, pastel o galleta)").toLowerCase();
 
-/*Aquí vas a pedir nombre y años, para después concatenarlos con las variables anteriores
-y mostrarlas con un alert y / o log*/
-let ingresaNombre = prompt("Ingrese su nombre");
-let ingresaAños = prompt("¿Cuántos años tiene?");
+    switch (PRODUCTS) {
+        case "pan": console.log(amount = 12);
+            break;
+        case "pastel": console.log(amount = 300);
+            break;
+        case "galleta": console.log(amount = 8);
+            break;
+        default: alert("Eso no lo manejamos, lo sentimos, se le cobrarán 3 pesos"); amount = 3;
+    }
+    totalAmount += amount;
+    console.log("El total es de $" + totalAmount);
+    alert("El total es de $" + totalAmount);
 
-let sentenciaCompleta = textoParaNombre + espacioBlanco + ingresaNombre + espacioBlanco + textoUnion + espacioBlanco + ingresaAños + espacioBlanco + textoParaAños;
-console.log(sentenciaCompleta);
-alert(sentenciaCompleta);
+    const HAVE_DISCOUNT = prompt("¿Tiene un código de descuento? (si / no").toLowerCase();
+    if (HAVE_DISCOUNT === "si") {
+        let discountCode = prompt("Ingreselo").toUpperCase();
+        switch (discountCode) {
+            case "MIAPAESELDUEÑO": console.log(discountAmount = 85);
+                break;
+            case "AYUDITA": console.log(discountAmount = 15);
+                break;
+            default: alert("Ese código no es válido.");
+        }
+
+        function makeDiscount(total, discount) {
+            let disc = (total * discount) / 100;
+            let totalMinusDisc = total - disc;
+            console.log("El total con descuento es de $" + totalMinusDisc);
+            alert("El total con descuento es de $" + totalMinusDisc);
+        }
+
+        makeDiscount(totalAmount, discountAmount);
+
+    } else { alert("Que pena, para la próxima quizás.") }
+
+    continueDoing = prompt("¿Desea seguir comprando? (responda si / no)").toLowerCase();
+} while (continueDoing === "si");
